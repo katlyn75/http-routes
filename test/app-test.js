d84bec77e91adcac9e0e7a67b1bd0ac4a3e47ed0
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-16 17:21:24 -0700

    wrote first test

--- test/app-test.js
+++ test/app-test.js
@@ -1,15 +1,23 @@
-const dotenv = require('dotenv');
+//const dotenv = require('dotenv');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+chai.use = (chaiHttp);
+const { assert }  = chai;
 
-const client = require('./lib/database-client');
-const app = require('/lib/app');
-
-const assert = chai;
-//chai.use = chai(http);
 
 describe('table of rocks', () => {
 
+    it('gets a rock from table', () => {
+        /*Pass app to chai.request, which will start (call.listen()) and auto close server after request*/
+        return chai.request(app)
+        //method and path
+            .get('/')
+            .then(response => {
+                assert.equal(response.text, rock);
+            
+            });
+    });
+    
     //puts rock into list
     before(() => client.query('INSERT INTO rocks'));
 
@@ -32,10 +40,6 @@ describe('table of rocks', () => {
     };
 
     
-    it('saves a rock to table', () => {
-        return chai.request(app);
-        assert.equal(rock);
-    });
 
 });
 
