839a326d326af0a65468e7face21e7d725c2ee63
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-17 01:22:54 -0700

    created database client and create

--- test/app-test.js
+++ test/app-test.js
@@ -1,31 +1,41 @@
-//const dotenv = require('dotenv');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use = (chaiHttp);
 const { assert }  = chai;
+const app = require('../lib/app');
 
 
 describe('table of rocks', () => {
 
-    it('gets a rock from table', () => {
+    it('gets a rock from the table on GET /', () => {
         /*Pass app to chai.request, which will start (call.listen()) and auto close server after request*/
         return chai.request(app)
         //method and path
             .get('/')
             .then(response => {
-                assert.equal(response.text, rock);
+                assert.equal(response.text, 'rock');
             
             });
     });
-    
-    //puts rock into list
-    before(() => client.query('INSERT INTO rocks'));
 
-    let lapis = {
-        name: 'lapis',
-        color: 'cobalt',
-        category_id: 1
-    };
+    it('returns specific rock on GET /rock', () => {
+        return chai.request(app)
+            .get('/rock')
+            .then(response => {
+                assert.deepEqual(response.text, {
+                    
+                    name: 'lapis',
+                    color: 'cobalt',
+                    category_id: 1
+                });
+            });
+    });
+    
+});
+
+//puts rock into list
+/*before(() => client.query('INSERT INTO rocks'));
+
 
     let amethyst = {
         name: 'amethyst',
@@ -37,11 +47,10 @@ describe('table of rocks', () => {
         name: 'flourite',
         color: 'blue',
         category_id: 1
-    };
+    };*/
 
     
 
-});
 
 
 
